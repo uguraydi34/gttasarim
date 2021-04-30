0a1e3238d81aafd8fe736c7c3dde941375f5c05e
uguraydi34 <uguraydi34@gmail.com>
2021-04-30 16:10:41 +0300

    containder ve logo yerleştirme rotate ekleme

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    BenimUyg(),
+  );
+}
+
+class BenimUyg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-    backgroundColor: Colors.blueGrey[100],
-    appBar: AppBar(
-      centerTitle: true,
-      title: Text("Güler Tasarım"),
-      backgroundColor: Colors.pinkAccent[300],
-    ),
-    body: Center(
-      child: Image.asset("images/gtlogo.png"),
-    ),
-  )));
+        backgroundColor: Colors.blueGrey[100],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Güler Tasarım"),
+          backgroundColor: Colors.pinkAccent[300],
+        ),
+        body: SafeArea(
+          child: Container(
+            //transform: Matrix4.rotationZ(-0.2),
+            width: 200,
+            height: 150,
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(
+                50), //margin dışardaki elemanlar ile arasına mesafe  yaratır.
+            padding: EdgeInsets.all(
+                5), //elemanın sınırlarından kendi sınırlarına boşluk oluşturur.
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.deepPurple[200],
+              borderRadius: BorderRadiusDirectional.circular(16.0),
+            ),
+            child: Transform(
+                transform: Matrix4.rotationZ(-0.2),
+                alignment: FractionalOffset.center,
+                child: Image.asset("images/gtlogo.png")),
+          ),
+        ),
+      ),
+    );
+  }
 }
