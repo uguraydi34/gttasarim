bdae0ab1b0ed9956db55415a17b93f42e447926f
uguraydi34 <uguraydi34@gmail.com>
2021-05-03 10:53:03 +0300

    uygulama başlangıcı row ve column ornekleri

--- lib/main.dart
+++ lib/main.dart
@@ -12,30 +12,58 @@ class BenimUyg extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey[100],
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Güler Tasarım"),
-          backgroundColor: Colors.pinkAccent[300],
-        ),
+        // appBar: AppBar(
+        //  centerTitle: true,
+        // title: Text("Güler Tasarım"),
+        //backgroundColor: Colors.pinkAccent[300],
+        // ),
         body: SafeArea(
-          child: Container(
-            //transform: Matrix4.rotationZ(-0.2),
-            width: 200,
-            height: 150,
-            alignment: Alignment.center,
-            margin: EdgeInsets.all(
-                50), //margin dışardaki elemanlar ile arasına mesafe  yaratır.
-            padding: EdgeInsets.all(
-                5), //elemanın sınırlarından kendi sınırlarına boşluk oluşturur.
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.deepPurple[200],
-              borderRadius: BorderRadiusDirectional.circular(16.0),
-            ),
-            child: Transform(
-                transform: Matrix4.rotationZ(-0.2),
-                alignment: FractionalOffset.center,
-                child: Image.asset("images/gtlogo.png")),
+          child: Column(
+            //mainAxisSize: MainAxisSize.min, //en az ekranda yer kapla demek
+            //mainAxisAlignment: MainAxisAlignment.start, //Ana ekranın başından itibaren yukarıdan aşağı çizer varsayılan oludu için bişi değişmiyecek
+            //mainAxisAlignment:MainAxisAlignment.end, //elemanalrı en alta yakın dizer.
+            //mainAxisAlignment: MainAxisAlignment.center, //elemanları ortalar.
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //elemanalrın aralarında eşit boşluk bırakır
+            crossAxisAlignment: CrossAxisAlignment.center,
+            //yatayda ekranı doldurmasu için CrossAxisAligment.Stretch yazmak yeterli
+            //bunu yaparsak alttaki widt degerleri geçersiz olur.
+            children: [
+              //bu elemanların arasında boşluk oluşturmak istersek sizedbox height değeri ilede verebiliriz.
+              Container(
+                //transform: Matrix4.rotationZ(-0.2),
+                width: 200,
+                height: 150,
+                alignment: Alignment.center,
+                //margin: EdgeInsets.all(15), //margin dışardaki elemanlar ile arasına mesafe  yaratır.
+                //padding: EdgeInsets.all(5), //elemanın sınırlarından kendi sınırlarına boşluk oluşturur.
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.deepPurple[200],
+                  borderRadius: BorderRadiusDirectional.circular(16.0),
+                ),
+                child: Transform(
+                    transform: Matrix4.rotationZ(-0.2),
+                    alignment: FractionalOffset.center,
+                    child: Image.asset("images/gtlogo.png")),
+              ),
+              Container(
+                width: 200,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.pinkAccent,
+                  borderRadius: BorderRadiusDirectional.circular(16.0),
+                ),
+                child: Text("data"),
+              ),
+              Container(
+                width: double.infinity,
+                //ekran el verdiğince alabileceği en yüksek değer
+                //bu conrainerı oluşturmamızdaki sebep ekrana ortalaması için
+                //crossAxisAligment.center ın çalışması için .
+              )
+            ],
           ),
         ),
       ),
